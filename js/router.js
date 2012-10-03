72a98c908d8f0ceb310d208d1eac98ca97b567fa
Thomas Davis <thomasalwyndavis@gmail.com>
2012-10-03 11:26:18 +0000

    more examples

--- js/router.js
+++ js/router.js
@@ -8,6 +8,9 @@ define([
   var AppRouter = Backbone.Router.extend({
     routes: {
 
+      'html-templates': 'htmlpage',
+      'ajax-data': 'ajaxpage',
+      'social-media-data': 'socialpage',
       '': 'home'
     }
   });
@@ -17,6 +20,24 @@ define([
 		var appView = options.appView;
     var router = new AppRouter(options);
     Backbone.router = router;
+    router.on('route:htmlpage', function () {
+      require(['views/examples/html'], function(HtmlPage){
+        var htmlPage = new HtmlPage();
+        htmlPage.render();
+      })
+    });    
+    router.on('route:ajaxpage', function () {
+      require(['views/examples/json'], function(JsonPage){
+        var jsonPage = new JsonPage();
+        jsonPage.render();
+      })
+    });    
+    router.on('route:socialpage', function () {
+      require(['views/examples/social'], function(SocialPage){
+        var socialPage = new SocialPage();
+        socialPage.render();
+      })
+    });
     router.on('route:home', function () {
       require(['views/home/page'], function(HomePage){
         var homePage = new HomePage();
